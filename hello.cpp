dd35351ec760a34861866ec415058fd80c4f27d5
Paul Schuetze <paul.schuetze@desy.de>
2021-03-01 11:52:07 +0100

    Make command line argument as name input possible. No argument still triggers keyboard input

--- hello.cpp
+++ hello.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void sayHello(const char*);
-void sayGoodbye(const char*);
+void sayHello(std::string);
+void sayGoodbye(std::string);
 
-int main(){
+int main(int argc, char *argv[]){
 
-    char name[100];
-    cout << "Gib deinen Namen ein: ";
-    cin >> name;
-    cout << endl;
+    bool interactive = !(argc == 2);
+    
+    std::string name;
+
+    if(interactive) {
+	cout << "Gib deinen Namen ein: ";
+	cin >> name;
+	cout << endl;
+    } else {
+	name = argv[1];
+    }
     
     sayHello(name);
     sayGoodbye(name);
 }
 
 // New function
-void sayHello(const char* name){
+void sayHello(std::string name){
     cout << "Hallo " << name << "!" << endl;
 }
 
-void sayGoodbye(const char* name){
+void sayGoodbye(std::string name){
     cout << "Tschuess " << name << "!" << endl;
 }
